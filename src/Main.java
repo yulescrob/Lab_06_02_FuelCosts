c8300a0dce22c96c2e82f3e1c81a5cdcc43da37f
yulescrob <122058516+yulescrob@users.noreply.github.com>
2023-02-19 22:31:07 -0500

    revised proj

--- src/Main.java
+++ src/Main.java
@@ -14,45 +14,42 @@ public class Main {
         if(in.hasNextDouble()){
             gasInTank = in.nextDouble();
             in.nextLine();
+
+            System.out.print("Enter the fuel efficiency in miles per gallon: ");
+            if(in.hasNextDouble()){
+                fuelEfficiency = in.nextDouble();
+                in.nextLine();
+
+                System.out.print("Enter the price of gas per gallon: ");
+                if(in.hasNextDouble()){
+                    gasPrice = in.nextDouble();
+                    in.nextLine();
+
+                    //process calculations
+                    costPer100Mi = 100 * gasPrice / fuelEfficiency;
+                    distanceWithGasTank = fuelEfficiency * gasInTank;
+
+                    //output
+
+                    System.out.println("The cost of gas per 100 miles is: " +costPer100Mi);
+                    System.out.println("With the gas in the tank the car go for " + distanceWithGasTank + " miles.");
+                }
+                else {
+                    trash = in.nextLine();
+                    System.out.println("Must enter a valid number for the price of gas per gallon " + trash);
+                    //error message if wrong input
+                }
+            }
+            else {
+                trash = in.nextLine();
+                System.out.println("Must enter a valid number for the fuel efficiency " + trash);
+                //error message if wrong input
+            }
         }
         else {
             trash = in.nextLine();
             System.out.println("Must enter a valid number for the number of gallons of gas in the tank " + trash);
         //error message if wrong input
         }
-
-        System.out.print("Enter the fuel efficiency in miles per gallon: ");
-        if(in.hasNextDouble()){
-            fuelEfficiency = in.nextDouble();
-            in.nextLine();
-        }
-        else {
-            trash = in.nextLine();
-            System.out.println("Must enter a valid number for the fuel efficiency " + trash);
-            //error message if wrong input
-        }
-
-        System.out.print("Enter the price of gas per gallon: ");
-        if(in.hasNextDouble()){
-            gasPrice = in.nextDouble();
-            in.nextLine();
-        }
-        else {
-            trash = in.nextLine();
-            System.out.println("Must enter a valid number for the price of gas per gallon " + trash);
-            //error message if wrong input
-        }
-
-        //process calculations
-        costPer100Mi = 100 * gasPrice / fuelEfficiency;
-        distanceWithGasTank = fuelEfficiency * gasInTank;
-
-        //output
-        System.out.println("The cost of gas per 100 miles is: " +costPer100Mi);
-        System.out.println("With the gas in the tank the car go for " + distanceWithGasTank + " miles.");
-
-
-
-
     }
 }
